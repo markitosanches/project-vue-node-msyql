b2442c4c5e7680fe34617fa7862d4919e67e3d14
Marcosm <markitosanches@gmail.com>
2022-03-13 12:17:21 -0400

    init vuejs project

--- server/server.js
+++ server/server.js
@@ -5,24 +5,24 @@ const app = express()
 var corsOptions = {
   origin: 'http://localhost:8081'
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 // parse requests of content-type - application/json
-const db = require("./app/models");
-db.sequelize.sync();
+const db = require('./app/models')
+db.sequelize.sync()
 //  used for development
 //  db.sequelize.sync({ force: true }).then(() => {
-//     console.log("Drop and re-sync db.");
+//     console.log("Drop and re-sync db.")
 //   });
 app.use(bodyParser.json())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // simple route just for test
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome your server is running :)" })
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome your server is running :)' })
 })
 // set port, listen for requests
 
-require("./app/routes/product.route")(app);
+require('./app/routes/product.route')(app);
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
